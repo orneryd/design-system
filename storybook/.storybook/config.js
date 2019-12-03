54c618738329d5b67e6454e6692ca355bd7aead9
Luis Laborda <luis.laborda@mckesson.com>
2019-12-03 11:20:37 -0600

    Adds UI Testing and Snapshots

--- storybook/.storybook/config.js
+++ storybook/.storybook/config.js
@@ -1,10 +1,21 @@
 import { configure, addParameters } from '@storybook/react';
-import mckTheme from './mckTheme';
-//
-addParameters({
-  options: {
-    theme: mckTheme,
-  },
-})
+import requireContext from 'require-context.macro';
+import '../src/index.css';
+import '../src/components/Typography/Typography.css';
+
+// import mckTheme from './mckTheme';
+// //
+// addParameters({
+//   options: {
+//     theme: mckTheme,
+//   },
+// })
 // automatically import all files ending in *.stories.js
-configure(require.context('../src', true, /\.stories\.js$/), module);
+// configure(require.context('../src', true, /\.stories\.js$/), module);
+const req = requireContext('../src', true, /\.stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
