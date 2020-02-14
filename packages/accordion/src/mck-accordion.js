482dba07840bd2aefe5b5434e94f8449021e1f4d
Sweet, Timothy <Timothy.Sweet@McKesson.com>
2020-02-14 12:34:46 -0700

    reworking accordion to suck less

--- packages/accordion/src/mck-accordion.js
+++ packages/accordion/src/mck-accordion.js
@@ -8,39 +8,30 @@ export default class MckAccordion extends HTMLElement {
   }
 
   onHeaderClick() {
-    const contentSlot = this.shadowRoot.querySelector('.mck-accordion-content')
-    const headerIcon = this.shadowRoot.querySelector('.mck-accordion-header-icon')
-    const isOpen = !contentSlot.style.height
-    if (isOpen) {
-      headerIcon.classList.add('open')
-    } else {
-      headerIcon.classList.remove('open')
-    }
-    const eventOptions = { detail: isOpen ? 'open' : 'collapse' }
+    const isOpen = this.getAttribute('state') === 'open'
+    const eventOptions = { detail: isOpen ? 'collapse' : 'open'}
     this.setAttribute('state', eventOptions.detail)
     this.dispatchEvent(new CustomEvent('accordion-state', eventOptions))
-    contentSlot.style.height = isOpen ? contentSlot.scrollHeight + 'px' : null
+  }
 
-    const remove = () => {
-      contentSlot.removeEventListener('transitionend', remove)
-    }
-    contentSlot.addEventListener('transitionend', remove)
+  toggleCollapse(){
+    const contentSlot = this.shadowRoot.querySelector('.mck-accordion-content')
+    const slotContents = this.querySelector('[slot=accordion-content]')
+    const headerIcon = this.shadowRoot.querySelector('.mck-accordion-header-icon')
+    const isOpen = this.getAttribute('state') === 'open'
+    // val can be either false or number
+    const val = (isOpen && 0 || slotContents.scrollHeight)
+    // if val is false, javascript evalues the next expression in the ternary 
+    // and returns that value regardless of the truthyness.
+    const newHeight = (val || 0)
+    // now we always end up with a number.
+    contentSlot.style.height = `${newHeight}px`
+    isOpen ? headerIcon.classList.add('open') : headerIcon.classList.remove('open')
   }
 
   connectedCallback() {
     accordionTemplate(this).connect()
-    this.mutationsObserver_ = new MutationObserver(() => {
-      const accordionContent = this.shadowRoot.querySelector('.mck-accordion-content')
-      const slotContents = this.querySelector('[slot=accordion-content]')
-      const isOpen = this.getAttribute('state') === 'open'
-      const headerIcon = this.shadowRoot.querySelector('.mck-accordion-header-icon')
-      if (isOpen) {
-        headerIcon.classList.add('open')
-        accordionContent.style.height = slotContents.scrollHeight + 'px'
-      } else {
-        headerIcon.classList.remove('open')
-      }
-    })
+    this.mutationsObserver_ = new MutationObserver(() => this.toggleCollapse())
     this.mutationsObserver_.observe(this, {
       attributes: true,
       childList: true,
