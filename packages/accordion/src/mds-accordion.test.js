bf0e30768f98d211dbf3c8504bc729d2713c5010
Sweet, Timothy <Timothy.Sweet@McKesson.com>
2020-02-14 17:17:22 -0700

    adding unit-test placeholder for now

---
+++ packages/accordion/src/mds-accordion.test.js
@@ -0,0 +1,71 @@
+/**
+ * @jest-environment jest-environment-happy-dom
+ */
+import './mds-accordion'
+
+describe('MdsAccordion', () => {
+  let element
+  beforeEach(() => {
+    element = document.createElement('mds-accordion')
+    element.setAttribute('state', 'open')
+    element.innerHTML = `
+    <div slot="accordion-header">
+      test-text
+    </div>
+    <div slot="accordion-content">
+      test-text2
+    </div>
+    `
+    document.body.appendChild(element)
+  })
+
+  afterEach(() => document.body.removeChild(element))
+
+  it('should have a shadow root', () => {
+    expect(element.shadowRoot).toBeDefined()
+  })
+
+  describe('onHeaderClick', () => {
+    let dispatchSpy
+    beforeEach(() => {
+      element.parentElement = {
+        removeChild: jest.fn()
+      }
+      dispatchSpy = spyOn(element, 'dispatchEvent')
+      element.onHeaderClick()
+    })
+
+    afterEach(() => {
+      jest.clearAllMocks()
+    })
+
+    afterAll(() => {
+      element.dispatchEvent.mockRestore()
+    })
+
+    it('should dispatch a single event', () => {
+      expect(dispatchSpy.calls.count()).toBe(1)
+    })
+
+    it('should call dispatchEvent on the element with the onHeaderClick custom event', () => {
+      expect(dispatchSpy.calls.mostRecent().args[0].type).toBe('accordion-state')
+    })
+
+    // it('should call dispatchEvent on the element with the detail being the eventOptions value', () => {
+    //   expect(dispatchSpy.calls.mostRecent().args[0].detail).toBe('open')
+    // })
+  })
+  describe('connectedCallback', () => {
+    it('should have a style element', () => {
+      const styleNodes = element.shadowRoot.querySelectorAll('style')
+      expect(styleNodes.length).toBe(1)
+      expect(styleNodes[0].tagName).toBe('STYLE')
+    })
+
+    it('should have a div element', () => {
+      const mckChipNodes = element.shadowRoot.querySelectorAll('.mds-accordion')
+      expect(mckChipNodes.length).toBe(1)
+      expect(mckChipNodes[0].tagName).toBe('DIV')
+    })
+  })
+})
