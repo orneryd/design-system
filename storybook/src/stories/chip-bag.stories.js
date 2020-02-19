5fd44493154b4bab086089ac5c65fdd46ed4fb0c
Sweet, Timothy <Timothy.Sweet@McKesson.com>
2020-02-19 09:34:07 -0700

    adding example to storybook

---
+++ storybook/src/stories/chip-bag.stories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import '../../../packages/paper/dist/mds-paper'
+import '../../../packages/chip-bag/dist/mds-chip-bag'
+
+storiesOf('Web Components | Inputs', module).add('Chip Bag', () => (
+  <mds-paper>
+    <mds-chip-bag value="sample;chips" delimiter=";">
+      <div slot="chip-bag-helper">Add some chips!</div>
+    </mds-chip-bag>
+  </mds-paper>
+))
