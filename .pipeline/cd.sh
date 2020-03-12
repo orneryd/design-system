85cf84378e4c95f0a133dd1b74edaaf8d6fbf2be
Sweet, Timothy <Timothy.Sweet@McKesson.com>
2020-03-12 16:56:09 -0700

    update cd script

--- .pipeline/cd.sh
+++ .pipeline/cd.sh
@@ -10,6 +10,9 @@ handle_error() {
 trap 'handle_error $LINENO' ERR
 
 echo 'Deploying to development'
+cp -av ../dist ./build/dist
+cp -av ./public/samples ./build/samples
+cp -av ./public/simone ./build/simone
 cf login -a "${PCF_DEV_API}" -u "${PCF_USER}" -p "${PCF_PASSWORD}" -o ISRM-SecOps-dev -s dev
 
 cf push -f manifest.dev.yml
