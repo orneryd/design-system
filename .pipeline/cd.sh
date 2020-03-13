ed4324973806742703d51fc798bfbe34d4581368
Ruairi O'Brien <ruairi.obrien@mckesson.com>
2020-03-13 15:35:45 +0000

    Uncomment push

--- .pipeline/cd.sh
+++ .pipeline/cd.sh
@@ -14,4 +14,4 @@ trap 'handle_error $LINENO' ERR
 echo 'Deploying to development'
 cf login -a "${PCF_DEV_API}" -u "${PCF_USER}" -p "${PCF_PASSWORD}" -o ISRM-SecOps-dev -s dev
 
-#cf push -f manifest.dev.yml
+cf push -f manifest.dev.yml
